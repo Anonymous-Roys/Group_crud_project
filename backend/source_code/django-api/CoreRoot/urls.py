45332b4f30c81d1b53e019846923d8b693bd94db
YOUNG KIRK <adjanour@icloud.com>
2023-12-17 21:48:36 +0000

    modified directory structure -- breaking changes

--- backend/source_code/django-api/CoreRoot/urls.py
+++ backend/source_code/django-api/CoreRoot/urls.py
@@ -14,15 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.db import router
-from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView,  SpectacularSwaggerView
-from rest_framework.routers import DefaultRouter
-from core.routers import router as core_router
-from ecommerce.routers import router as ecommerce_router
-from ecommerce.routers import urlpatterns as ecommerce_urlpatterns
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path,include
+from core.routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
