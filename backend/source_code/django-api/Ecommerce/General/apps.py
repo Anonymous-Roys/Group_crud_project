68be37344d10976f4d43627aa003d223aa16aba3
YOUNG KIRK <adjanour@icloud.com>
2023-12-20 13:55:07 +0000

    added image upload functionality

--- backend/source_code/django-api/Ecommerce/General/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ecommerce.General'
-    label = 'ecommerce_General'
